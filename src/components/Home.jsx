715e9365a5b1fb0b7a98c11e18102da33678af5d
darewill <urimm.r@gmail.com>
2024-11-18 20:45:47 +0100

    background image add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,11 +6,12 @@ import { Button } from "../components/ui/button";
 export default function Home() {
   return (
     <div className='home-wrapper'>
-      <div className="search-bar flex w-full max-w-sm items-center space-x-2">
-        <Input type="email" placeholder="Email" />
-        <Button type="submit">Search</Button>
+      <h1 className='text-[35px] text-white flex justify-center uppercase font-bold mb-[50px] font-sans'>Search a player by their username or id</h1>
+      <div className="search-container flex items-center justify-center">
+        <Input type="text" placeholder="Players Username / ID" className='search-bar rounded-r-none font-semibold'/>
+        <Button variant="destructive" className='search-btn rounded-l-none font-semibold'>Search</Button>
       </div>
-      <div className="display">
+      <div className="flex justify-center">
         <p className='text-white'>stuff</p>
       </div>
     </div>
